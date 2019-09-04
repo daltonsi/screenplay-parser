2f3da8211dea02d0e0fd21761ff747e7c673c8df
Dalton Simancek <daltonsimancek@Dalton-MacBook-Pro-Work.local>
2019-09-03 18:41:13 -0700

    add usage function

--- screenplay_parser.py
+++ screenplay_parser.py
@@ -1,1 +1,26 @@
-import re
+import argparse as ap
+import sys
+
+
+def usage():
+    print """
+    
+    *****
+    SCREENPLAY PARSER
+    *****
+    
+    Command line Usage:
+    
+    python screenplay_parser.py -c config.yml 
+    
+    
+    
+    
+    
+    """"
+
+
+
+
+
+    return sys.exit(1)
